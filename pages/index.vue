b098e32eb47069892307c4771dbc4b6f686b4650
queirozdaniel <danielqueirozak77@gmail.com>
2022-05-19 20:02:12 -0300

    feat: Request data with gql and using fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-divider></v-divider>
-    <!-- {{ categories }} -->
+    {{ $fetchState }}
     <div v-for="category in categories" :key="category.id">
       <v-btn 
         :to="{
@@ -12,53 +12,40 @@
         {{category.attributes.name}}
       </v-btn>
     </div>
-    <!-- <ApolloQuery :query="query">
-      <template slot-scope="{result: { data, loading, error }}">
-        <div v-if="loading">Carregando dados...</div>
-        <div v-else-if="error">{{error}}</div>
-        <div v-else v-for="category in data.categories.data" :key="category.id">
-          <v-btn 
-            :to="{
-              name: 'category', 
-              params : { category:category.attributes.slug },
-              query: { id: category.id}
-            }">
-            {{category.attributes.name}}
-          </v-btn>
-        </div>
-      </template>
-    </ApolloQuery> -->
   </v-container>
 </template>
 
 <script>
-// import { categoriesGql } from '@/graphql/queries'
 
 export default {
   name: 'IndexPage',
-  async asyncData(context) {
-    const client = context.app.apolloProvider.defaultClient
+  data() {
+    return {
+      categories: []
+    }
+  },
+  async fetch() {
+    const client = this.$apollo.getClient()
     const query = {
       query:require('../graphql/categories.gql')
     }
 
-    let categories = []
     await client.query(query).then( ({data}) => {
-      categories = data.categories.data
+      this.categories = data.categories.data
     })
+  }
+  // async asyncData(context) {
+  //   const client = context.app.apolloProvider.defaultClient
+  //   const query = {
+  //     query:require('../graphql/categories.gql')
+  //   }
 
-    return { categories }
-  },
-  // data() {
-  //   return {
-  //     query: categoriesGql
-  //   }
+  //   let categories = []
+  //   await client.query(query).then( ({data}) => {
+  //     categories = data.categories.data
+  //   })
+
+  //   return { categories }
   // },
-  // apollo: {
-  //   categories: {
-  //     query:  categoriesGql,
-  //     update: data => data.categories
-  //   }
-  // }
 }
 </script>
