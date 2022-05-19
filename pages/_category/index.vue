53b35f5ced367f216af1808d4ea5e1b6e5c40fce
queirozdaniel <danielqueirozak77@gmail.com>
2022-05-19 19:40:25 -0300

    feat: Request data with gql file

--- pages/_category/index.vue
+++ pages/_category/index.vue
@@ -2,6 +2,11 @@
   <div>
     {{ $route.params.category }}
     {{ category }}
+    <div>
+      {{ counter }}
+      <v-btn @click="callMutation()">Mutation</v-btn>
+      <v-btn @click="callAction()">Action</v-btn>
+    </div>
   </div>
 </template>
 
@@ -22,6 +27,19 @@ export default {
       variables() {
         return { id: this.$route.query.id}
       }
+    }
+  },
+  computed: {
+    counter() {
+      return this.$store.getters.readCounter
+    }
+  },
+  methods: {
+    callMutation() {
+      this.$store.commit('increment')
+    },
+    callAction(){
+      this.$store.dispatch('increment')
     }
   }
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
   <v-container>
-    {{ categories }}
     <v-divider></v-divider>
-    <ApolloQuery :query="query">
+    <!-- {{ categories }} -->
+    <div v-for="category in categories" :key="category.id">
+      <v-btn 
+        :to="{
+          name: 'category', 
+          params : { category:category.attributes.slug },
+          query: { id: category.id}
+        }">
+        {{category.attributes.name}}
+      </v-btn>
+    </div>
+    <!-- <ApolloQuery :query="query">
       <template slot-scope="{result: { data, loading, error }}">
         <div v-if="loading">Carregando dados...</div>
         <div v-else-if="error">{{error}}</div>
@@ -17,26 +27,38 @@
           </v-btn>
         </div>
       </template>
-    </ApolloQuery>
+    </ApolloQuery> -->
   </v-container>
 </template>
 
 <script>
-import { categoriesGql } from '@/graphql/queries'
-
+// import { categoriesGql } from '@/graphql/queries'
 
 export default {
   name: 'IndexPage',
-  data() {
-    return {
-      query: categoriesGql
+  async asyncData(context) {
+    const client = context.app.apolloProvider.defaultClient
+    const query = {
+      query:require('../graphql/categories.gql')
     }
+
+    let categories = []
+    await client.query(query).then( ({data}) => {
+      categories = data.categories.data
+    })
+
+    return { categories }
   },
-  apollo: {
-    categories: {
-      query:  categoriesGql,
-      update: data => data.categories
-    }
-  }
+  // data() {
+  //   return {
+  //     query: categoriesGql
+  //   }
+  // },
+  // apollo: {
+  //   categories: {
+  //     query:  categoriesGql,
+  //     update: data => data.categories
+  //   }
+  // }
 }
 </script>
