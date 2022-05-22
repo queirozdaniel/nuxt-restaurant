1e47e65ab8f3bc17c63128573938e93e001b0483
queirozdaniel <danielqueirozak77@gmail.com>
2022-05-22 00:27:10 -0300

    feat: Created Component to display Recipes

---
+++ components/app/ui/NavCardRecipe.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card :to="{
+      name: 'category-recipe',
+      params: { category: recipe.attributes.category.data.attributes.slug , recipe: recipe.id}
+    }">
+    <v-img :src="recipe.attributes.img"></v-img>
+    <v-card-text>
+      <v-row>
+        <v-col cols="7">
+          <h3>{{ recipe.attributes.name }}</h3>
+        </v-col>
+        <v-col cols="5" class="d-flex justify-end">
+          <div>
+            <v-icon>mdi-heart</v-icon>
+            <span>{{ recipe.attributes.likes }}</span>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+  props: {
+    recipe: {
+      type: Object,
+      required: true
+    }
+  }
+
+}
+</script>
--- pages/_category/index.vue
+++ pages/_category/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <div v-for="recipe in recipes" :key="recipe.id">
+  <v-container>
+    <h1 class="secundary--text">
+      {{ category.attributes.name }}
+      <v-icon large class="secundary--text">{{ category.attributes.icon }}</v-icon>
+    </h1>
+    <p class="secundary--text">{{ category.attributes.description }}</p>
+    <v-row>
+      <v-col cols="4" v-for="recipe in recipes" :key="recipe.id">
+        <app-ui-nav-card-recipe :recipe="recipe"></app-ui-nav-card-recipe>
+      </v-col>
+    </v-row>
+    <!-- <div v-for="recipe in recipes" :key="recipe.id">
       <nuxt-link 
         :to="{
           name: 'category-recipe',
@@ -9,10 +19,10 @@
       > 
         {{ recipe.attributes.name }}
       </nuxt-link>
-    </div>
+    </div> -->
     <v-divider></v-divider>
     <nuxt-link to="/">Voltar</nuxt-link>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -20,6 +30,12 @@
 export default {
   data() {
     return {
+    }
+  },
+  computed: {
+    category() {
+      const slug = this.$route.params.category
+      return this.$store.getters.readCategories.find(category => category.attributes.slug == slug)
     }
   },
   async asyncData({app, route}) {
