b7b660e3e755ba61843c8712ada8316fb0873909
queirozdaniel <danielqueirozak77@gmail.com>
2022-05-24 23:54:13 -0300

    feat: Added a Register Component

--- components/app/form/Register.vue
+++ components/app/form/Register.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    Register
-  </div>
+  <v-card>
+    <v-card-title class="title primary white--text mb-3">
+      <v-icon class="mr-3" dark>mdi-account-plus</v-icon>
+      Criar Conta
+    </v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-text-field dense outlined label="Nome do usuário"></v-text-field>
+        <v-text-field dense outlined label="E-mail"></v-text-field>
+        <v-text-field dense outlined label="Senha"></v-text-field>
+        <v-text-field dense outlined label="Confirmar senha"></v-text-field>
+        <div class="d-flex justify-space-between">
+          <v-btn color="secundary" small >Entrar</v-btn>
+          <v-btn color="secundary" small outlined @click="close()">Fechar</v-btn>
+        </div>
+      </v-container>
+    </v-card-text>
+  </v-card>
 </template>
+
+<script>
+export default {
+  methods: {
+    close() {
+      this.$emit('close',false)
+    }
+  }
+}
+</script>
