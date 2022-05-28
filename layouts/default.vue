7405445883bf60a61d1139d281f5e2844184653f
queirozdaniel <danielqueirozak77@gmail.com>
2022-05-28 20:47:28 -0300

    feat: Added a component to see user logged

--- layouts/default.vue
+++ layouts/default.vue
@@ -61,8 +61,37 @@
         </v-card>
       </v-menu>
       <div v-if="$auth.loggedIn">
-        {{ $auth.user.username }}
         <v-btn text color="primary" @click="logout()">Sair</v-btn>
+        <v-menu
+          v-model="loginmenu"
+          :close-on-content-click="false"
+          offset-y
+        >
+          <template v-slot:activator="{on}">
+            <v-btn v-on="on" color="secundary" icon>
+                <v-avatar>
+                  <span class=" headline">{{ $auth.user.username[0] }}</span>
+                </v-avatar>
+              </v-btn>
+          </template>
+          <v-card>
+            <v-list>
+              <v-list-item>
+                <v-list-content class="mx-2">
+                  <v-list-item-title>{{ $auth.user.username }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ $auth.user.email }}</v-list-item-subtitle>
+                </v-list-content>
+                <v-list-action>
+                  <v-btn to="/user" small>Editar</v-btn>
+                </v-list-action>
+              </v-list-item>
+            </v-list>
+            <v-card-actions>
+              <v-btn text color="primary" @click="loginmenu = false" small>Fechar</v-btn>
+              <v-btn color="primary" @click="logout()">Sair de conta</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
       </div>
       <div v-else>
         <v-btn text color="primary" @click="dialog = true; type = 'app-form-login'">Acessar</v-btn>
@@ -117,7 +146,8 @@ export default {
       findRecipe: '',
       dialog: false,
       type: 'app-form-register',
-      snackbar: false
+      snackbar: false,
+      loginmenu: false
     }
   },
   computed: {
