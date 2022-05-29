d5e67c9b5de13e1f9c622e94369c38e72eab2f2c
queirozdaniel <danielqueirozak77@gmail.com>
2022-05-29 20:30:47 -0300

    feat: Added Component to register an user

--- components/app/form/Register.vue
+++ components/app/form/Register.vue
@@ -6,14 +6,16 @@
     </v-card-title>
     <v-card-text>
       <v-container>
-        <v-text-field dense outlined label="Nome do usuário"></v-text-field>
-        <v-text-field dense outlined label="E-mail"></v-text-field>
-        <v-text-field dense outlined label="Senha"></v-text-field>
-        <v-text-field dense outlined label="Confirmar senha"></v-text-field>
-        <div class="d-flex justify-space-between">
-          <v-btn color="secundary" small >Entrar</v-btn>
-          <v-btn color="secundary" small outlined @click="close()">Fechar</v-btn>
-        </div>
+        <v-form @submit.prevent="onsubmit" ref="form">
+          <v-text-field dense outlined label="Nome do usuário" v-model="userInfo.username" :rules="[rules.required]"></v-text-field>
+          <v-text-field dense outlined label="E-mail" v-model="userInfo.email" :rules="[rules.required, rules.email]"></v-text-field>
+          <v-text-field dense outlined label="Senha" v-model="userInfo.password" :rules="[rules.required, rules.min, rules.password]" counter></v-text-field>
+          <v-text-field dense outlined label="Confirmar senha" v-model="userInfo.repassword" :rules="[rules.required, rules.min, rules.password]" counter></v-text-field>
+          <div class="d-flex justify-space-between">
+            <v-btn color="secundary" small type="submit">Registrar-se</v-btn>
+            <v-btn color="secundary" small outlined @click="close()">Fechar</v-btn>
+          </div>
+        </v-form>
       </v-container>
     </v-card-text>
   </v-card>
@@ -21,9 +23,40 @@
 
 <script>
 export default {
+  data() {
+    return {
+      rules: {
+        required:(value) => !!value || "Campo obrigatório.",
+        min:(value) => value.length >= 8 || "Deve conter no mínimo 8 caracteres.",
+        email:(value) => /.+@/.test(value) || "Deve ser um e-mail.",
+        password:(value) => (this.userInfo.password === this.userInfo.repassword) || "As senhas não conferem."
+      },
+      userInfo: {
+        username: "",
+        email: "",
+        password: "",
+        repassword: ""
+      }
+    }
+  },
   methods: {
     close() {
       this.$emit('close',false)
+    },
+    onsubmit(){
+      if(this.$refs.form.validate()){
+        this.userInfo.identifier = this.userInfo.username
+        this.$apollo.mutate({
+          mutation:require('../../../graphql/register.gql'),
+          variables:this.userInfo
+        }).then( res => {
+          this.$auth.loginWith("local", { data: this.userInfo, })
+            .then(() => {
+              this.$emit('close',false)
+              this.$store.dispatch("snackbars/setSnack", { text: `Bem vindo, ${this.$auth.user.username}!` , color: 'success'})
+            })
+        })
+      }
     }
   }
 }
