a92752ea089747d9cd5f3961118a83255c8e563c
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-01 23:02:23 -0300

    feat: Created a view for Author see your recipes

---
+++ components/app/ui/ListRecipes.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-if="recipes.length != 0">
+      <v-list>
+        <v-list-item-group>
+          <template v-for="recipe in recipes">
+            <v-list-item two-line :key="recipe.id">
+              <v-list-item-content>
+                <v-list-item-title>{{recipe.attributes.name}}</v-list-item-title>
+                <v-list-item-subtitle>{{recipe.attributes.category.data.attributes.name}}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-row align="center" justify="center">
+                  <v-btn icon><v-icon>mdi-pencil</v-icon></v-btn>
+                  <v-btn icon><v-icon>mdi-delete</v-icon></v-btn>
+                </v-row>
+              </v-list-item-action>
+            </v-list-item>
+          </template>
+        </v-list-item-group>
+      </v-list>
+    </div>
+    <div v-else>
+      Não há receitas vinculadas.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recipes: {
+      type: Array,
+      default: () => ([])
+    }
+  }
+}
+</script>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    Bem Vindo
-  </div>
+  <v-container>
+    <h2>Bem vindo, {{ $auth.user.username }}</h2>
+    <v-divider class="my-5"></v-divider>
+    <h3>Suas Receitas</h3>
+    <v-btn color="primary" small class="my-3">Associar Receita</v-btn>
+    <div v-if="recipes">
+      <app-ui-list-recipes :recipes="recipes"></app-ui-list-recipes>
+    </div>
+    <div v-else></div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  // middleware : ''
+  middleware : 'auth',
+  async asyncData({app}){
+    let client = app.apolloProvider.defaultClient
+    let id = app.$auth.user.id
+
+    const query = {
+      query:require('../../graphql/userRecipes.gql'),
+      variables:{id}
+    }
+
+    let recipes = null
+    await client.query(query).then(({ data }) => {
+      recipes = data.recipes.data
+    })
+    return {
+      recipes
+    }
+  }
 }
 </script>
